643a9c20864494bd6324cffcc6658ada35c00fe2
QuyTurtle12 <Ngocquyquy29@gmail.com>
2024-06-16 15:37:35 +0700

    Update AuthController.java

--- Jelwelry Sale System At The Store/src/main/java/com/swp391/jewelrysalesystem/controllers/AuthController.java
+++ Jelwelry Sale System At The Store/src/main/java/com/swp391/jewelrysalesystem/controllers/AuthController.java
@@ -4,11 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
 import com.swp391.jewelrysalesystem.models.AuthenticationRequest;
 import com.swp391.jewelrysalesystem.models.AuthenticationResponse;
 import com.swp391.jewelrysalesystem.models.User;
