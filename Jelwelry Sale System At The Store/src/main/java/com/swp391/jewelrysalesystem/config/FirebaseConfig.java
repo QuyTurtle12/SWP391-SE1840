2179da45e0c2d73a1a418bcc0721d0bca82ac5c8
QuyTurtle12 <Ngocquyquy29@gmail.com>
2024-06-22 12:11:14 +0700

    New service key

--- Jelwelry Sale System At The Store/src/main/java/com/swp391/jewelrysalesystem/config/FirebaseConfig.java
+++ Jelwelry Sale System At The Store/src/main/java/com/swp391/jewelrysalesystem/config/FirebaseConfig.java
@@ -17,7 +17,7 @@ public class FirebaseConfig {
     @PostConstruct
     public void initialize() throws IOException {
         try (InputStream serviceAccountStream = getClass().getClassLoader()
-                .getResourceAsStream("swp291-7bd29-firebase-adminsdk-f8mz9-8187f4cb93.json")) {
+                .getResourceAsStream("swp291-7bd29-firebase-adminsdk-f8mz9-e2c94c2a1f.json")) {
             if (serviceAccountStream == null) {
                 throw new IllegalArgumentException("Firebase service account key file not found.");
             }
