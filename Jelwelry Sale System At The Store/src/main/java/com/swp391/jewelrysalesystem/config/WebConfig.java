c19600089e64f3f8cb91f90d731f5868da45ab45
QuyTurtle12 <Ngocquyquy29@gmail.com>
2024-07-05 12:46:36 +0700

    change CORS origin

--- Jelwelry Sale System At The Store/src/main/java/com/swp391/jewelrysalesystem/config/WebConfig.java
+++ Jelwelry Sale System At The Store/src/main/java/com/swp391/jewelrysalesystem/config/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Chỉ định các origin cho phép truy cập, hoặc "*" cho phép tất cả
+                // .allowedOrigins("http://localhost:3000") // Chỉ định các origin cho phép truy cập, hoặc "*" cho phép tất cả
+                .allowedOrigins("https://jewelry-sale-system.vercel.app/") // Chỉ định các origin cho phép truy cập, hoặc "*" cho phép tất cả
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Phương thức HTTP cho phép
                 .allowedHeaders("*") // Các header cho phép
                 .allowCredentials(true); // Cho phép sử dụng cookie khi truy cập từ origin khác
