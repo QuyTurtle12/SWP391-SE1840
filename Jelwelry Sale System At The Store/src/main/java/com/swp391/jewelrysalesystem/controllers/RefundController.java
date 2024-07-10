c433aba6c234158444b85fae26409a81a34b0d6b
QuyTurtle12 <Ngocquyquy29@gmail.com>
2024-07-10 13:04:10 +0700

    modified getRefundList

--- Jelwelry Sale System At The Store/src/main/java/com/swp391/jewelrysalesystem/controllers/RefundController.java
+++ Jelwelry Sale System At The Store/src/main/java/com/swp391/jewelrysalesystem/controllers/RefundController.java
@@ -110,7 +110,7 @@ public class RefundController {
 
         List<Map<String, Object>> response = new ArrayList<>();
         for (Refund refund : refunds) {
-            String customerName = customerService.getCustomer(refund.getID()).getName();
+            String customerName = customerService.getCustomer(refund.getCustomerID()).getName();
             String staffName = userService.getUserByField(refund.getStaffID(), "id", "user").getFullName();
 
             Map<String, Object> map = new HashMap<>();
